62fa2cb90c6a575079fb92e80593ad91824594fb
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-09-29 15:13:20 +0900

    Prettier 설정
    자동으로 코드 포맷을 맞춰줌 좋은듯?
    
    이거 외에 ESLint 도 있는데 google, airbnb 설정을 따라할 수 있음
    근데 너무 까다로워서 협업하는게 아니라면 기본 설정만으로도 충분하다고 함

--- src/App.js
+++ src/App.js
@@ -1,96 +1,99 @@
 import React, { useReducer, useCallback, useRef, useMemo } from 'react';
-import './App.css'
+import './App.css';
 
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import useInputs from './Hook/useInputs';
 
-
-function countActiveUsers(users){
+function countActiveUsers(users) {
   console.log('유저 세는 중');
-  return users.filter(user => user.active).length;
+  return users.filter((user) => user.active).length;
 }
 
 const initialState = {
-  users : [
+  users: [
     {
       id: 1,
       userName: 'velopert',
       email: 'public.velopert@gmail.com',
-      active : true
+      active: true,
     },
     {
       id: 2,
       userName: 'tester',
       email: 'tester@example.com',
-      active : false
+      active: false,
     },
     {
       id: 3,
       userName: 'liz',
       email: 'liz@example.com',
-      active : false
-    }
-  ]
-}
+      active: false,
+    },
+  ],
+};
 
-function reducer(state, action){
-
-  switch(action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case 'CREATE_USER':
       return {
-        users : state.users.concat(action.user)
+        users: state.users.concat(action.user),
       };
-    
+
     case 'TOGGLE_USER':
       return {
-        users : state.users.map(user => 
-          user.id === action.id ? {...user, active: !user.active} : user
-        )
-      }
-    
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user,
+        ),
+      };
+
     case 'REMOVE_USER':
       return {
-        users : state.users.filter(user => 
-          user.id !== action.id
-        )
-      }
-    
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+
     default:
       return state;
   }
-
 }
 
 export const UserDispatch = React.createContext(null);
 
+// function App() {
+
+//   // const [{ userName, email }, onChange, reset] = useInputs({
+//   //   userName : '',
+//   //   email : ''
+//   // });
+
+//   const [ state, dispatch ] = useReducer(reducer, initialState);
+//   const { users } = state;
+
+//   // const onCreate = useCallback(()=>{
+
+//   // }, [userName, email, reset]);
+
+//   const count = useMemo(()=>countActiveUsers(users), [users]);
+
+//   return (
+
+//   <UserDispatch.Provider value={dispatch}>
+
+//                   <CreateUser
+//                   />
+//       <UserList users={users}/>
+//       <div>활성사용자 수 : {count}</div>
+//     </ UserDispatch.Provider>
+//   );
+// }
+
 function App() {
-
-  // const [{ userName, email }, onChange, reset] = useInputs({
-  //   userName : '',
-  //   email : ''
-  // });
-
-  const [ state, dispatch ] = useReducer(reducer, initialState);
-  const { users } = state;
-  
-
-
-  // const onCreate = useCallback(()=>{
-    
-  // }, [userName, email, reset]);
-
-
-  const count = useMemo(()=>countActiveUsers(users), [users]);
+  const a = 'hello';
 
   return (
-    <UserDispatch.Provider value={dispatch}>
-    
-      <CreateUser 
-      />
-      <UserList users={users}/>
-      <div>활성사용자 수 : {count}</div>
-    </ UserDispatch.Provider>
+    <div>
+      <p>와우</p>
+    </div>
   );
 }
 
